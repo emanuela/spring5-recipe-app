ff0a6c732c2761b0915a6d6f81b5a2ddf589796f
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-06-04 14:53:05 -0600

    Added Recipe and Notes and the relatonship between them.

---
+++ src/main/java/com/emanuel/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,28 @@
+/*
+ * COGNIZANT CONFIDENTIAL AND/OR TRADE SECRET
+ *
+ * Copyright [2015] - [2020] Cognizant.  All rights reserved.
+ *
+ * NOTICE: This unpublished material is proprietary to Cognizant and
+ * its suppliers, if any.  The methods, techniques and technical
+ * concepts herein are considered Cognizant confidential ane/or trade
+ * secret information.  This material may be covered by U.S. and/or
+ * foreign patents or patent applications.  Use, distribution or
+ * copying, in whole or in part, is forbidden, except by express written
+ * permission of Cognizant.
+ *
+ */
+
+package com.emanuel.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/emanuel/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,61 @@
+/*
+ * COGNIZANT CONFIDENTIAL AND/OR TRADE SECRET
+ *
+ * Copyright [2015] - [2020] Cognizant.  All rights reserved.
+ *
+ * NOTICE: This unpublished material is proprietary to Cognizant and
+ * its suppliers, if any.  The methods, techniques and technical
+ * concepts herein are considered Cognizant confidential ane/or trade
+ * secret information.  This material may be covered by U.S. and/or
+ * foreign patents or patent applications.  Use, distribution or
+ * copying, in whole or in part, is forbidden, except by express written
+ * permission of Cognizant.
+ *
+ */
+
+package com.emanuel.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
