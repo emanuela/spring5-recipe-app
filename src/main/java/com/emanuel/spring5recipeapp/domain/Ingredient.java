49b198db7b2c0ecc1282abf79260ba9808008cf7
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-06-05 10:58:02 -0600

    Add a One To Many relationship from Recipe to Ingredient and Many To One from Ingredient to Recipe.

---
+++ src/main/java/com/emanuel/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,70 @@
+/*
+ * COGNIZANT CONFIDENTIAL AND/OR TRADE SECRET
+ *
+ * Copyright [2015] - [2020] Cognizant.  All rights reserved.
+ *
+ * NOTICE: This unpublished material is proprietary to Cognizant and
+ * its suppliers, if any.  The methods, techniques and technical
+ * concepts herein are considered Cognizant confidential ane/or trade
+ * secret information.  This material may be covered by U.S. and/or
+ * foreign patents or patent applications.  Use, distribution or
+ * copying, in whole or in part, is forbidden, except by express written
+ * permission of Cognizant.
+ *
+ */
+
+package com.emanuel.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    // private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
--- src/main/java/com/emanuel/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/emanuel/spring5recipeapp/domain/Recipe.java
@@ -21,7 +21,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -39,6 +41,9 @@ public class Recipe {
     private String directions;
     //todo add
     // private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -124,5 +129,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
