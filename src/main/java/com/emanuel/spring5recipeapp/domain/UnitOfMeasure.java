00bf88895b4374ccf86af7122870f12b6547b67b
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-06-05 11:28:38 -0600

    Add a One To One relationship from Ingredient to UnitOfMeasure.

--- src/main/java/com/emanuel/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/com/emanuel/spring5recipeapp/domain/UnitOfMeasure.java
@@ -16,28 +16,18 @@
 package com.emanuel.spring5recipeapp.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import java.math.BigDecimal;
 
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
-    private BigDecimal amount;
-
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
-
-    @ManyToOne
-    private Recipe recipe;
 
     public Long getId() {
         return id;
@@ -53,29 +43,5 @@ public class Ingredient {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
     }
 }
